2a58d029f20a29adf4e05b91f56180121297af1c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-08-06 11:24:12 +0900

    variables and constants

--- tutorial_go/README.md
+++ tutorial_go/README.md
@@ -6,3 +6,18 @@ go run main.go
 
 - 함수명 - 대문자 -> export -> import
 - 함수명 - 소문자 -> private
+
+----
+
+- go 는 타입 언어
+- const 상수 ( 변경 불가 )
+- var 변수
+
+```
+var name string = "john"
+-> name := "john"
+
+func 안에서만 사용가능함, 변수에만 사용가능
+축약형은 처음 변수로 부터 타입을 추론
+
+```
--- tutorial_go/main.go
+++ tutorial_go/main.go
@@ -9,4 +9,15 @@ func main() {
 	fmt.Println("main")
 	something.SayHello()
 
+	const name string = "John"
+	// name = "Jane" 불가
+	fmt.Println(name)
+	
+	var name2 string = "john"
+	name2 = "jane"
+	fmt.Println(name2)
+
+	name3 := "john"
+	name3 = "jane"
+	fmt.Println(name3)
 }
