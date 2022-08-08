afe46bbcae6773cd49e8a6b3c02e152563c5da2e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-08-09 01:54:05 +0900

    function, 변수 축약 타입, 리턴, 인수 무제한

--- tutorial_go/main.go
+++ tutorial_go/main.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"tutorial_go/something"
 )
 
+// func multiply(a, int, b int) int {}
+// 축약
+func multiply(x, y int) int {
+	return x * y
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+ 
 func main() {
 	fmt.Println("main")
+
+	fmt.Println(multiply(2, 3))
+
+	totalLength, upperName := lenAndUpper("hello123");
+	fmt.Println(totalLength, upperName)
+
+	totalLength2, _ := lenAndUpper("bye123");
+	fmt.Println(totalLength2)
+
+
+	repeatMe("aa","bb","cc")
+
 	something.SayHello()
 
 	const name string = "John"
