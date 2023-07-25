6669e7bc62ecbf35ed32533095b7e4e2945b8505
wonhopark89-dev <wonhopark89.dev@gmail.com>
2023-07-26 02:01:08 +0900

    add goroutines

--- url_checker/main.go
+++ url_checker/main.go
@@ -4,37 +4,41 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
 	//  make: 빈배열 생성
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-	}
+	// var results = make(map[string]string)
+	// urls := []string{
+	// 	"https://www.airbnb.com/",
+	// 	"https://www.google.com/",
+	// 	"https://www.amazon.com/",
+	// 	"https://www.reddit.com/",
+	// 	"https://www.google.com/",
+	// 	"https://soundcloud.com/",
+	// 	"https://www.facebook.com/",
+	// 	"https://www.instagram.com/",
+	// }
 
-	for _, url := range urls {
-		result := "OK"
-		err := hitUrl(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
+	// for _, url := range urls {
+	// 	result := "OK"
+	// 	err := hitUrl(url)
+	// 	if err != nil {
+	// 		result = "FAILED"
+	// 	}
+	// 	results[url] = result
+	// }
 
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	// for url, result := range results {
+	// 	fmt.Println(url, result)
+	// }
 
+	go helloCount("hello")
+	go helloCount("bye")
+	time.Sleep(time.Second * 5)
 }
 
 func hitUrl(url string) error {
@@ -45,4 +49,11 @@ func hitUrl(url string) error {
 		return errRequestFailed
 	}
 	return nil
+}
+
+func helloCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is count", i)
+		time.Sleep(time.Second)
+	}
 }
