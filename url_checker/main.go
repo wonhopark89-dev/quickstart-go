8042ee5de7cb69c77fef008fa39944b90c029901
wonhopark <61549673+wonhopark89-dev@users.noreply.github.com>
2023-07-27 01:27:22 +0900

    add goroutines (#1)
    * add url_checker
    
    * add goroutines

---
+++ url_checker/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	//  make: 빈배열 생성
+	// var results = make(map[string]string)
+	// urls := []string{
+	// 	"https://www.airbnb.com/",
+	// 	"https://www.google.com/",
+	// 	"https://www.amazon.com/",
+	// 	"https://www.reddit.com/",
+	// 	"https://www.google.com/",
+	// 	"https://soundcloud.com/",
+	// 	"https://www.facebook.com/",
+	// 	"https://www.instagram.com/",
+	// }
+
+	// for _, url := range urls {
+	// 	result := "OK"
+	// 	err := hitUrl(url)
+	// 	if err != nil {
+	// 		result = "FAILED"
+	// 	}
+	// 	results[url] = result
+	// }
+
+	// for url, result := range results {
+	// 	fmt.Println(url, result)
+	// }
+
+	go helloCount("hello")
+	go helloCount("bye")
+	time.Sleep(time.Second * 5)
+}
+
+func hitUrl(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+}
+
+func helloCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is count", i)
+		time.Sleep(time.Second)
+	}
+}
