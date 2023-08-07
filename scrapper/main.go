abf2a799fa71e08baf6341ae079796eaa5cb3517
wonhopark89-dev <wonhopark89.dev@gmail.com>
2023-08-08 02:57:10 +0900

    goquery with struct

--- scrapper/main.go
+++ scrapper/main.go
@@ -10,17 +10,52 @@ import (
 
 var baseUrl string = "https://kr.iherb.com/new-products?"
 
+type extractedItem struct {
+	id    string
+	title string
+}
+
 func main() {
+	var items []extractedItem
 	totalPage := getPages()
 
 	for i := 1; i <= totalPage; i++ {
-		getPage(i)
+		items = append(items, getPage(i)...)
+	}
+
+	// fmt.Println(items)
+	for _, item := range items {
+		fmt.Println(item.id, item.title)
 	}
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedItem {
+	var items []extractedItem
 	pageURL := baseUrl + "p=" + strconv.Itoa(page)
-	fmt.Println("Requesting", pageURL)
+	// fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".product-cell-container").Each(func(i int, item *goquery.Selection) {
+		id, _ := item.Find(".product").Attr("itemid")
+
+		item.Find(".product-inner>div").Each(func(i int, item *goquery.Selection) {
+			content, exist := item.Find(".product-title").Attr("content")
+			if exist {
+				// fmt.Println(id, content)
+				items = append(items, extractedItem{
+					id:    id,
+					title: content,
+				})
+			}
+		})
+		// fmt.Println(id)
+	})
+	return items
 }
 
 func getPages() int {
