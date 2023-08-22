49f9d00f6f389394762258bf5422e903e9b98ecf
wonhopark89-dev <wonhopark89.dev@gmail.com>
2023-08-23 02:21:59 +0900

    add file download and remove on server

--- scrapper/main.go
+++ scrapper/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo/v4"
+	"os"
+	"scrapper/scrapper"
 	"strings"
 )
+
+const fileName string = "items.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName) // 서버(폴더)에 있는 파일 삭제하기 위해 defer 사용 ( 다운로드 후 삭제 )
 	term := strings.ToLower(c.FormValue("term"))
-	fmt.Println(term)
-	fmt.Println(len(term))
-	return nil
+	// fmt.Println(term)
+	// fmt.Println(len(term))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
