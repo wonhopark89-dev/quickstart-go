05b1415a22d946da2da3fe0f8bcaa7082983c817
wonhopark89-dev <wonhopark89.dev@gmail.com>
2023-08-23 02:13:18 +0900

    echo with html

--- scrapper/main.go
+++ scrapper/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "scrapper/scrapper"
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"strings"
+)
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(c.FormValue("term"))
+	fmt.Println(term)
+	fmt.Println(len(term))
+	return nil
+}
 
 func main() {
-	scrapper.Scrape("vita")
+	// scrapper.Scrape("vita")
+
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+
 }
